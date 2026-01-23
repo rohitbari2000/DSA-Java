dc175b0e7ec0bc18067d8f1d03f3f96972ebfc89
Rohit Bari <rohitbari2000@gmail.com>
2026-01-23 16:09:04 +0530

    Added RowColMatrix solution

---
+++ Pattern-wise-questions/src/binarysearch/RowColMatrix.java
@@ -0,0 +1,31 @@
+package binarysearch;
+
+import java.util.Arrays;
+
+// A row column matrix is the one sorted rowWise and columnWise
+public class RowColMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {28, 29, 37, 49},
+                {33, 34, 38, 50}
+        };
+        System.out.println(Arrays.toString(search(matrix, 97)));
+    }
+
+    static int[] search(int[][] matrix, int target){
+        int row = 0;
+        int column = matrix.length - 1;
+
+        while(row <= matrix.length - 1 && column >= 0){
+            if(matrix[row][column] == target)
+                return new int[]{row, column};
+            if (matrix[row][column] < target)
+                row++;
+            else
+                column--;
+        }
+        return new int[]{-1, -1};
+    }
+}
